d6d263afc2cb4af02a7fea0f9e9bff42340af659
huykevin2002 <giahuy300336@gmail.com>
2022-12-16 18:36:46 +0700

    Commit 6
    - Add category to each Field table

--- middlewares/locals.mdw.js
+++ middlewares/locals.mdw.js
@@ -4,7 +4,10 @@ import fieldService from '../services/field.service.js';
 export default function (app) {
     app.use(async function (req, res, next) {
         res.locals.lcFields = await fieldService.findAll();
-        res.locals.lcFields[0].lcCategories = await categoryService.findAllByField(res.locals.lcFields[0].fieldID);
+        let fieldLen = res.locals.lcFields.length;
+        for (let i = 0; i < fieldLen; i++) {
+            res.locals.lcFields[i].lcCategories = await categoryService.findAllByField(res.locals.lcFields[i].fieldID);
+        }
         // res.locals.lcCategories = await categoryService.findAll();
 
         next();
