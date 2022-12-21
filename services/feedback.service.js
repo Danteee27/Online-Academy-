b93393b56d76d1fece7d61f6b0983df1c458407c
huykevin2002 <giahuy300336@gmail.com>
2022-12-21 13:59:32 +0700

    Commit 17
    - Fix css
    - Fix service feedback

--- services/feedback.service.js
+++ services/feedback.service.js
@@ -8,5 +8,20 @@ export default {
     async findByCourseID(courseID) {
         const list = await db('feedbacks').where('courseID', courseID);
         return list;
+    },
+
+    findByCourseIDWithLimit(courseID, limit) {
+        return db('feedbacks').where('courseID', courseID).limit(limit);
+    },
+
+    findByCourseIDWithLimitOffset(courseID, limit, offset) {
+        return db('feedbacks').where('courseID', courseID).limit(limit).offset(offset);
+    },
+
+    async countByCourseID(courseID) {
+        const list = await db('feedbacks').where('courseID', courseID).count({
+            amount: 'fbID'
+        });
+        return list[0].amount;
     }
 };
