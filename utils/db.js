aac9841950238cb691e855b137ed9e2174432ac2
Danteee27 <duongducanh6101@gmail.com>
2023-01-07 16:42:46 +0700

    Cap quyen teacher tu Admin

--- utils/db.js
+++ utils/db.js
@@ -10,7 +10,7 @@ const knex = knexObj({
         host: '127.0.0.1',
         port: 3306,
         user: 'root',
-        password: 'root',
+        password: process.env.DB_PASSWORD,
         database: 'qlkh'
     },
     pool: {
