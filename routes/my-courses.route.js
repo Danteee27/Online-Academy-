dcbf456e32068da09a3682db53d97aa365452929
huykevin2002 <giahuy300336@gmail.com>
2023-01-09 17:07:48 +0700

    update views and routes
    Wishlist:
    - view: col-3 to col-2
    - route: paging limit from 8 to 12
    My-courses:
    - same as Wishlist

--- routes/my-courses.route.js
+++ routes/my-courses.route.js
@@ -1,58 +1,56 @@
-import express from 'express';
-import courseService from '../services/courses.service.js';
-import myCourseService from '../services/my-courses.service.js';
-import teachersService from '../services/teachers.service.js';
+import express from "express";
+import courseService from "../services/courses.service.js";
+import myCourseService from "../services/my-courses.service.js";
+import teachersService from "../services/teachers.service.js";
 
 const router = express.Router();
 
 router.get("/", async function (req, res) {
-    if (req.session.authUser === null) {
-        return res.redirect('/');
-    }
+  if (req.session.authUser === null) {
+    return res.redirect("/");
+  }
 
-    res.locals.lcMyCoursePage = true;
-    res.locals.lcTitle = "My Courses | " + res.locals.lcTitle;
+  res.locals.lcMyCoursePage = true;
+  res.locals.lcTitle = "My Courses | " + res.locals.lcTitle;
 
-    const userID = res.locals.authUser.userID;
+  const userID = res.locals.authUser.userID;
 
-    const total = await myCourseService.countByUserID(userID);
-    const limit = 8;
-    let nPages = Math.floor(total / limit);
-    if (total % limit > 0) {
-        nPages++;
-    }
-    const page = req.query.page || 1;
-    const offset = (page - 1) * limit;
-    const pageNumbers = [];
-    for (let i = 1; i <= nPages; i++) {
-        pageNumbers.push({
-            value: i,
-            isCurrent: +page === i
-        });
-    }
-
-    const list = await myCourseService.findByUserID(userID, limit, offset);
-
-    let course = [];
-    for (let i = 0; i < list.length; i++) {
-        let temp = await courseService.findByIdWithoutHidden(list[i].courseID);
-        if (temp === null)
-            continue;
-        let tempTeacher = await teachersService.findById(temp.teacherID);
-        if (tempTeacher !== null)
-            temp.instructor = tempTeacher.teacherName;
-        course.push(temp);
-    }
-
-    res.render('vwUser/my-courses', {
-        course,
-        pageNumbers,
-        empty: list.length === 0,
-        prevPage: +page - 1,
-        nextPage: +page + 1,
-        hasPrevPage: +page > 1,
-        hasNextPage: +page < nPages
+  const total = await myCourseService.countByUserID(userID);
+  const limit = 12;
+  let nPages = Math.floor(total / limit);
+  if (total % limit > 0) {
+    nPages++;
+  }
+  const page = req.query.page || 1;
+  const offset = (page - 1) * limit;
+  const pageNumbers = [];
+  for (let i = 1; i <= nPages; i++) {
+    pageNumbers.push({
+      value: i,
+      isCurrent: +page === i,
     });
+  }
+
+  const list = await myCourseService.findByUserID(userID, limit, offset);
+
+  let course = [];
+  for (let i = 0; i < list.length; i++) {
+    let temp = await courseService.findByIdWithoutHidden(list[i].courseID);
+    if (temp === null) continue;
+    let tempTeacher = await teachersService.findById(temp.teacherID);
+    if (tempTeacher !== null) temp.instructor = tempTeacher.teacherName;
+    course.push(temp);
+  }
+
+  res.render("vwUser/my-courses", {
+    course,
+    pageNumbers,
+    empty: list.length === 0,
+    prevPage: +page - 1,
+    nextPage: +page + 1,
+    hasPrevPage: +page > 1,
+    hasNextPage: +page < nPages,
+  });
 });
 
 export default router;
--- routes/wishlists.route.js
+++ routes/wishlists.route.js
@@ -1,78 +1,75 @@
-import express from 'express';
-import courseService from '../services/courses.service.js';
-import teachersService from '../services/teachers.service.js';
-import wishlistService from '../services/wishlists.service.js';
+import express from "express";
+import courseService from "../services/courses.service.js";
+import teachersService from "../services/teachers.service.js";
+import wishlistService from "../services/wishlists.service.js";
 
 const router = express.Router();
 
 router.get("/", async function (req, res) {
-    if (req.session.authUser === null) {
-        return res.redirect('/');
-    }
-    res.locals.lcWishlistPage = true;
-    res.locals.lcTitle = "Wishlist | " + res.locals.lcTitle;
-    const userID = res.locals.authUser.userID;
+  if (req.session.authUser === null) {
+    return res.redirect("/");
+  }
+  res.locals.lcWishlistPage = true;
+  res.locals.lcTitle = "Wishlist | " + res.locals.lcTitle;
+  const userID = res.locals.authUser.userID;
 
-    const total = await wishlistService.countByUserID(userID);
-    const limit = 8;
-    let nPages = Math.floor(total / limit);
-    if (total % limit > 0) {
-        nPages++;
-    }
-    const page = req.query.page || 1;
-    const offset = (page - 1) * limit;
-    const pageNumbers = [];
-    for (let i = 1; i <= nPages; i++) {
-        pageNumbers.push({
-            value: i,
-            isCurrent: +page === i
+  const total = await wishlistService.countByUserID(userID);
+  const limit = 12;
+  let nPages = Math.floor(total / limit);
+  if (total % limit > 0) {
+    nPages++;
+  }
+  const page = req.query.page || 1;
+  const offset = (page - 1) * limit;
+  const pageNumbers = [];
+  for (let i = 1; i <= nPages; i++) {
+    pageNumbers.push({
+      value: i,
+      isCurrent: +page === i,
+    });
+  }
+  const wishlist = await wishlistService.findByUserID(userID, limit, offset);
+
+  let course = [];
+  for (let i = 0; i < wishlist.length; i++) {
+    let temp = await courseService.findByIdWithoutHidden(wishlist[i].courseID);
+    if (temp === null) continue;
+    let tempTeacher = await teachersService.findById(temp.teacherID);
+    if (tempTeacher !== null) temp.instructor = tempTeacher.teacherName;
+    if (+temp.promotion != 0) temp.hasPromotion = true;
+    course.push(temp);
+  }
+  for (let i = 0; i < course.length; i++) {
+    const star = [];
+    for (let j = 1; j <= 5; j++) {
+      if (j <= +course[i].rating)
+        star.push({
+          star: true,
+          starHalf: false,
+        });
+      else if (j - +course[i].rating < 1)
+        star.push({
+          star: false,
+          starHalf: true,
+        });
+      else
+        star.push({
+          star: false,
+          starHalf: false,
         });
     }
-    const wishlist = await wishlistService.findByUserID(userID, limit, offset);
+    course[i].star = star;
+  }
 
-    let course = [];
-    for (let i = 0; i < wishlist.length; i++) {
-        let temp = await courseService.findByIdWithoutHidden(wishlist[i].courseID);
-        if (temp === null)
-            continue;
-        let tempTeacher = await teachersService.findById(temp.teacherID);
-        if (tempTeacher !== null)
-            temp.instructor = tempTeacher.teacherName;
-        if (+temp.promotion != 0)
-            temp.hasPromotion = true;
-        course.push(temp);
-    }
-    for (let i = 0; i < course.length; i++) {
-        const star = [];
-        for (let j = 1; j <= 5; j++) {
-            if (j <= +course[i].rating)
-                star.push({
-                    star: true,
-                    starHalf: false
-                });
-            else if (j - +course[i].rating < 1)
-                star.push({
-                    star: false,
-                    starHalf: true
-                });
-            else
-                star.push({
-                    star: false,
-                    starHalf: false
-                });
-        }
-        course[i].star = star;
-    }
-
-    res.render('vwUser/wishlists', {
-        course,
-        pageNumbers,
-        empty: wishlist.length === 0,
-        prevPage: +page - 1,
-        nextPage: +page + 1,
-        hasPrevPage: +page > 1,
-        hasNextPage: +page < nPages
-    });
+  res.render("vwUser/wishlists", {
+    course,
+    pageNumbers,
+    empty: wishlist.length === 0,
+    prevPage: +page - 1,
+    nextPage: +page + 1,
+    hasPrevPage: +page > 1,
+    hasNextPage: +page < nPages,
+  });
 });
 
 export default router;
